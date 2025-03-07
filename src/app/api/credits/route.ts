6002cf9b5baa73554b43c508a385e4e436ca5579
TheSupremeHackers <nicocavrod2012@gmail.com>
2025-03-07 17:13:22 +0100

    Update route.ts

--- src/app/api/credits/route.ts
+++ src/app/api/credits/route.ts
@@ -1,51 +1,17 @@
-// src/app/api/credits/route.ts
-
+// app/api/credits/route.ts
 import { NextResponse } from 'next/server'
 import { cookies } from 'next/headers'
 
 const CREDITS_COOKIE_NAME = 'nicogpt_credits'
-const LAST_RESET_COOKIE_NAME = 'nicogpt_last_reset'
+const DEFAULT_CREDITS = 50
 
-// Función para obtener los créditos almacenados en las cookies
-const getCredits = () => {
+const getAvailableCredits = () => {
   const cookieStore = cookies()
   const credits = cookieStore.get(CREDITS_COOKIE_NAME)?.value
-  return credits ? parseInt(credits, 10) : 50 // Valor por defecto de 50 créditos
-}
-
-// Función para actualizar los créditos en las cookies
-const setCredits = (credits: number) => {
-  const cookieStore = cookies()
-  cookieStore.set(CREDITS_COOKIE_NAME, credits.toString())
+  return credits ? parseInt(credits, 10) : DEFAULT_CREDITS
 }
 
 export async function GET() {
-  const credits = getCredits()
+  const credits = getAvailableCredits()
   return NextResponse.json({ credits })
 }
-
-export async function POST(req: Request) {
-  const body = await req.json()
-  const { action } = body
-
-  let credits = getCredits()
-
-  switch (action) {
-    case 'consume':
-      credits -= 1
-      break
-    case 'image':
-      credits -= 10
-      break
-    case 'music':
-      credits -= 15
-      break
-    default:
-      break
-  }
-
-  setCredits(credits)
-
-  return NextResponse.json({ credits })
-}
-
